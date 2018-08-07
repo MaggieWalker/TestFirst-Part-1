8758b8d6b2551bdd146bbcae3ddb74161d2988bf
Maggie Walker <mags21walker@gmail.com>
2018-08-07 13:49:08 -0400

    installed ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+    "extends": "fullstack",
+    "parser": "babel-eslint",
+    "rules": {
+    }
+}
---
+++ 01-Object-Properties/properties.js
@@ -0,0 +1,8 @@
+
+function setPropsOnObj(object) {
+    object.x = 7;
+    object.y = 8;
+    object.onePlus = function onePlus(i) {
+        i++;
+        return i;
+    }}
